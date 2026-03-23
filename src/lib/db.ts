b7f7a6ee6cf8567589b5f8a882bae83f1635e07d
Administrador <admin@eltemplo.com>
2026-03-23 15:35:52 -0600

    Support NETLIFY VARS

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { Pool } from 'pg';
 
 // El pool administra las conexiones de forma eficiente
-// Usa la variable de entorno DATABASE_URL configurada en .env.local
-const connectionString = process.env.DATABASE_URL || '';
+// Usa la variable de entorno configurada nativamente o por la extensión de Netlify
+const connectionString = process.env.DATABASE_URL || process.env.NETLIFY_DATABASE_URL || '';
 
 const pool = new Pool({
   connectionString,
